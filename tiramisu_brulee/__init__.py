103bd23f4d70acf833abf74a42aad379b3dcb064
Jacob Reinhold <jacob.reinhold@jhu.edu>
2021-05-13 12:54:03 -0400

    add version to toplevel init

--- tiramisu_brulee/__init__.py
+++ tiramisu_brulee/__init__.py
@@ -1,4 +1,9 @@
 # -*- coding: utf-8 -*-
+
+__author__ = """Jacob Reinhold"""
+__email__ = 'jcreinhold@gmail.com'
+__version__ = '0.1.0'
+
 from .loss import *
 from .model import *
 from .util import *
