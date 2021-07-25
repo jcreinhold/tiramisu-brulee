1dfdf617af8af84877adf4adf35c424a0209b366
Jacob Reinhold <jcreinhold@gmail.com>
2021-07-25 15:39:44 -0400

    Bump version: 0.1.19 → 0.1.20

--- setup.py
+++ setup.py
@@ -74,6 +74,6 @@ setup(
     test_suite="tests",
     tests_require=test_requirements,
     url="https://github.com/jcreinhold/tiramisu-brulee",
-    version="0.1.19",
+    version="0.1.20",
     zip_safe=False,
 )
