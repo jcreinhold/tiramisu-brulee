e3031e150d6b431432f3546cd165bce7255e0abf
Jacob Reinhold <5241441+jcreinhold@users.noreply.github.com>
2021-12-16 16:04:22 -0500

    Bump version: 0.1.36 → 0.1.37

--- tiramisu_brulee/__init__.py
+++ tiramisu_brulee/__init__.py
@@ -2,6 +2,6 @@ import logging
 
 __author__ = """Jacob Reinhold"""
 __email__ = "jcreinhold@gmail.com"
-__version__ = "0.1.36"
+__version__ = "0.1.37"
 
 logging.getLogger(__name__).addHandler(logging.NullHandler())
